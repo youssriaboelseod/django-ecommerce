a7465e6bba82c0f5ab5b755ecf834fdf2f2329fd
Youssri Ahmed <youssri.ahmed@cg-eg.com>
2026-05-31 16:07:48 +0300

    updgrade to django4

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -20,18 +20,21 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'django_countries',
-
+    'crispy_bootstrap4',
     'core'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'djecommerce.urls'
